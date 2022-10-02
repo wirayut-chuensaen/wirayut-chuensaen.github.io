4f60ad45ff84b942ff47f261540aa4cbdd73b64a
neostormz <rinz.neostormz@gmail.com>
2022-10-02 16:30:25 +0700

    fix mui theme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import ContextProvider from './Contexts/state'
 import Router from './Routes';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const THEME = createTheme({
+	typography: {
+		"fontFamily": `"IBM Plex Sans Thai", sans-serif`,
+		//  "fontSize": 14,
+		//  "fontWeightLight": 300,
+		//  "fontWeightRegular": 400,
+		//  "fontWeightMedium": 500
+	}
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<ContextProvider>
-		<Router />
-	</ContextProvider>
+	<ThemeProvider theme={THEME}>
+		<ContextProvider>
+			<Router />
+		</ContextProvider>
+	</ThemeProvider>
 );
